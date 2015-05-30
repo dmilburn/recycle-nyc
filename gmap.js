432b1aaebd36c19d3be1ceaebe7c7a1a401aefd1
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-30 17:07:53 -0400

    Separate out lat and lng

--- gmap.js
+++ gmap.js
@@ -1,7 +1,9 @@
 function initialize() {
+  var lat = 44.5403;
+  var lng = -78.5463
   var mapCanvas = document.getElementById('map-canvas');
   var mapOptions = {
-    center: new google.maps.LatLng(44.5403, -78.5463),
+    center: new google.maps.LatLng(lat, lng),
     zoom: 8,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
