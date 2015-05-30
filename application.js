7ca76b826e11f8e4d5881e23a4c107099b3758d8
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-30 19:01:35 -0400

    Create event listener for search bar submit

--- application.js
+++ application.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   initialize(40.696110, -73.995235);
+
+  $('#search').on('submit', function(event){
+    event.preventDefault();
+    console.log("hello");
+  });
+
 })
