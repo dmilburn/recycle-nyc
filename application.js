d5df8a1d4ad6be767ce9dfc5141e45854ac6dc57
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-30 21:14:33 -0400

    Hide google map until search occurs

--- application.js
+++ application.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-  initialize(40.696110, -73.995235);
+  $('#map-canvas').hide();
+
 
   $('#search').on('submit', function(event){
     event.preventDefault();
     var address = document.getElementById("address").value;
     var boroughForm = document.getElementById('borough');
     var borough = boroughForm.options[boroughForm.selectedIndex].text;
-    console.log(borough)
+    $('#map-canvas').show();
+    initialize(40.696110, -73.995235);
+
+
   });
 
 })
